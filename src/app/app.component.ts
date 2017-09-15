37a5531b29318fe67efae1d3fde40388aeac12dd
Jejomar <jejomardorongon@gmail.com>
2017-09-15 23:55:46 +0800

    Finished the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,16 @@ export class AppComponent {
     ];
   }
 
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
